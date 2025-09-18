90a9e0f1917aaa6943a2a41ef47ba2a7dfe3c8e7
containerscrew <containerscrew@users.noreply.github.com>
2025-09-19 00:50:52 +0200

    feat: preparing systemd service

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,29 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 
 # Compile the program
 make
 
-# Copy the bpf program into /usr/local/share/rootisnaked/rootisnaked.bpf.o
+BIN_NAME="bin/rootisnaked"
+SERVICE_NAME="systemd/rootisnaked.service"
+
+# Copy the bpf program
 sudo mkdir -p /usr/local/share/rootisnaked
 sudo cp build/rootisnaked.bpf.o /usr/local/share/rootisnaked/rootisnaked.bpf.o
 
-# Copy the binary into /usr/local/bin/rootisnaked
-sudo cp bin/rootisnaked /usr/local/bin/rootisnaked
+# Copy the binary
+sudo install -m 0755 "$BIN_NAME" /usr/local/bin/
 
-printf "Installation complete. You can run the program using 'export TELEGRAM_TOKEN=\"xxxxx\"; export DEBUG=true; export CHAT_ID=\"xxxxx\" ; sudo -E ./bin/rootisnaked'\n"
+# Copy systemd service
+sudo install -m 0644 "$SERVICE_NAME" /etc/systemd/system/
+
+# Create log directory and file
+sudo mkdir -p /var/log/rootisnaked
+sudo touch /var/log/rootisnaked/lastlog
+
+# Reload systemd and enable service
+sudo systemctl daemon-reload
+sudo systemctl enable --now rootisnaked.service
+
+printf "\nInstallation complete. Service 'rootisnaked' is installed and running.\n"
